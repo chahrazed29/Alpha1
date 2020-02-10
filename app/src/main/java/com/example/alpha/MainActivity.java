c878ad3e3edf1def3b6434028d456504a8847bd1
Serine07 <mzserine@gmail.com>
2020-02-10 21:25:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.alpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void addition(){
+        int i=1+1;
+    }
+}
