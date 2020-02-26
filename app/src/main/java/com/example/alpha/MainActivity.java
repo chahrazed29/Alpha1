adb21593f500cfc31c4d36393491d5222f1d3a89
Serine07 <mzserine@gmail.com>
2020-02-26 22:55:56 +0100

    Modified MainActivity

--- app/src/main/java/com/example/alpha/MainActivity.java
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.alpha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton=(Button)findViewById(R.id.buttonConnecter);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
 
 }
