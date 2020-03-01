5b40bfe0fc2aed710905fede5c3eca59eec98dd9
Serine07 <mzserine@gmail.com>
2020-03-01 21:51:32 +0100

    Modified MainActivity

--- app/src/main/java/com/example/alpha/LoginActivity.java
+++ app/src/main/java/com/example/alpha/LoginActivity.java
@@ -3,12 +3,28 @@ package com.example.alpha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public TextView insecrireTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        insecrireTextView=(TextView)findViewById(R.id.textInscrire2);
+        insecrireTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,IndEntActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/alpha/MainActivity.java
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -6,16 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     public Button loginButton;
+    public TextView insecrireTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        insecrireTextView=(TextView)findViewById(R.id.textInscrire);
+        insecrireTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,IndEntActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         loginButton=(Button)findViewById(R.id.buttonConnecter);
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
