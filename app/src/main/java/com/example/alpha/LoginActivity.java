4041cc600db1b28573912ee1187079a7d8658f99
Serine07 <mzserine@gmail.com>
2020-03-05 00:42:12 +0100

    Modified MainActivity

--- app/src/main/java/com/example/alpha/LoginActivity.java
+++ app/src/main/java/com/example/alpha/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
 import android.widget.ImageView;
@@ -12,6 +13,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public TextView insecrireTextView;
     ImageView backbutton;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         insecrireTextView=(TextView)findViewById(R.id.textInscrire2);
-
         insecrireTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(LoginActivity.this,IndEntActivity.class);
+                startActivity(intent);
+            }
+        });
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,PrincipaleActivity.class);
                 startActivity(intent);
             }
         });
@@ -35,6 +44,8 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
 
     }
 }
