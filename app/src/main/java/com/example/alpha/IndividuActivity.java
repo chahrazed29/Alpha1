e1141691e41acae8b7baae63c55318bdc69d3dc4
chahra29 <chahrazedhireche29@gmail.com>
2020-03-03 20:08:43 -0500

    splash act....

--- app/src/main/java/com/example/alpha/IndividuActivity.java
+++ app/src/main/java/com/example/alpha/IndividuActivity.java
@@ -6,31 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
-import android.widget.Button;
 
+import android.widget.ImageView;
 
 
 public class IndividuActivity extends AppCompatActivity {
-//public ImageView BackButton1;
-public Button b;
+//public ImageView backbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individu);
-        b.findViewById(R.id.backbtn1);
-        b.setOnClickListener(new View.OnClickListener() {
+       /*  backbutton.findViewById(R.id.backbtn);
+         backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
-        });
-    //BackButton1.findViewById(R.id.backbtn1);
-      /*  BackButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });*/
+        }); */
 
 
     }
---
+++ app/src/main/java/com/example/alpha/SplashscreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.alpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        //deriger vers la page principale
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //demarer une page
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        //handler post delay
+        new Handler().postDelayed(runnable,3000);
+    }
+
+}
