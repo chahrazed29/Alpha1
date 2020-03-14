1fcf73fef4c4b8712690a34eeaf6660772848bb9
chahra29 <chahrazedhireche29@gmail.com>
2020-03-14 07:14:29 -0400

    mapactivity

--- app/src/main/java/com/example/alpha/PrincipaleActivity.java
+++ app/src/main/java/com/example/alpha/PrincipaleActivity.java
@@ -7,17 +7,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-
+import com.google.android.gms.common.*;
+import com.google.android.gms.common.GoogleApiAvailability;
+import android.widget.Toast;
 import static com.example.alpha.R.string.navigation_drawer_close;
 import static com.example.alpha.R.string.navigation_drawer_open;
-
+import android.widget.Button;
+import android.content.Intent;
 public class PrincipaleActivity extends AppCompatActivity {
-    ImageView menubutton;
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private DrawerLayout drawer;
-   ActionBar.NavigationMode navigationModeView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +34,9 @@ public class PrincipaleActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer  ,navigation_drawer_open, navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        navigationModeView=findViewById(R.id.nav__view);
-        menubutton=findViewById(R.id.imageView37);
-        menubutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        if(isServicesOK()){
+            init();
+        }
     }
     @Override
     public void onBackPressed() {
@@ -42,6 +44,34 @@ public class PrincipaleActivity extends AppCompatActivity {
             drawer.closeDrawer(GravityCompat.START);
         }
         else{
-            super.onBackPressed();}
-    }
+            super.onBackPressed();}}
+    private void init(){
+        @SuppressLint("WrongViewCast") Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipaleActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }  public boolean isServicesOK(){
+            Log.d(TAG, "isServicesOK: checking google services version");
+
+            int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(PrincipaleActivity.this);
+
+            if(available == ConnectionResult.SUCCESS){
+                //everything is fine and the user can make map requests
+                Log.d(TAG, "isServicesOK: Google Play Services is working");
+                return true;
+            }
+            else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+                //an error occured but we can resolve it
+                Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+                Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(PrincipaleActivity.this, available, ERROR_DIALOG_REQUEST);
+                dialog.show();
+            }else{
+                Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
 }
